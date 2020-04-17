6a291fcdd4b856ea384659c80cee928103fe5b7a
AdmiralHonda <ceta17163@g.nihon-u.ac.jp>
2020-04-17 14:16:06 +0900

    hide sercret imformation

--- IizakaEmpire/IizakaEmpire/settings.py
+++ IizakaEmpire/IizakaEmpire/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from .secrets import sercret 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w-idd&50vv9uyx3-n0i-b4(b5e+6$r&y1zl*3ua865+^urzs_^'
+SECRET_KEY = sercret.CONNECTIONKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,12 +81,7 @@ WSGI_APPLICATION = 'IizakaEmpire.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = sercret.YOURDATABASECONFIG
 
 
 # Password validation
