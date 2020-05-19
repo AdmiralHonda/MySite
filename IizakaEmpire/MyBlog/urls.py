273609c7b3d4089c45a84ee18afddc44165089b1
AdmiralHonda <ceta17163@g.nihon-u.ac.jp>
2020-05-19 19:25:25 +0900

    [update]レイアウトの調整

--- IizakaEmpire/MyBlog/urls.py
+++ IizakaEmpire/MyBlog/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path,include
 
 from . import views
-from .views import index,blog
+from .views import Index, Blog, Categorys
 
 app_name='MyBlog'
 
 urlpatterns = [
-    path('',index.as_view(),name='index'),
-    path('blog/<slug:div>/<slug:slug>/',blog.as_view(),name='blog'),
-    path('search/<slug:type>/<int:searchtype>/',views.Categorys,name='search'),
-    path('sitepolicy/',views.sitepolicy,name='policy'),
-    path('markdownx/',include('markdownx.urls')),
+    path('', Index.as_view(), name='index'),
+    path('blog/<slug:div>/<slug:slug>/', Blog.as_view(), name='blog'),
+    path('search/<slug:kinds>/<int:search_type>/', Categorys.as_view(), name='search'),
+    path('sitepolicy/', views.sitepolicy, name='policy'),
+    path('markdownx/', include('markdownx.urls')),
 ]
--- IizakaEmpire/MyBlog/views.py
+++ IizakaEmpire/MyBlog/views.py
@@ -1,71 +1,55 @@
-from django.shortcuts import render,get_list_or_404,get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.core.paginator import Paginator
-from django.http import HttpResponse,Http404
-from django.views.generic import TemplateView,ListView,DetailView
-from .models import Article,Author,Category,Policy
+from django.http import HttpResponse, Http404
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Article, Author, Category, Policy,Tag
 # Create your views here.
 
-class index(ListView):
-    model=Article
-    template_name='index.html'
-    context_object_name='article'
+class Index(ListView):
+    model = Article
+    template_name = 'index.html'
+    context_object_name = 'article'
     paginate_by = 3
 
     def get_context_data(self,**kwargs):
-        context=super().get_context_data(**kwargs)
-        context['author']=Author.objects.all()
-
+        context = super().get_context_data(**kwargs)
+        context['author'] = Author.objects.all()
+        context['categorys'] = Category.objects.all()
         return context
 
 
-class blog(DetailView):
-    template_name='blog.html'
-    model=Article
-
-    def get_context_data(self,**kwargs):
-        context=super().get_context_data(**kwargs)
-        context['tags']=context['article'].tags.all()
-        context['recommend']=Article.objects.all()[:4]
+class Blog(DetailView):
+    template_name = 'blog.html'
+    model = Article
+ 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tags'] = context['article'].tags.all()
+        context['recommend'] = Article.objects.all()[:4]
+        context['categorys'] = Category.objects.all()
         return context
 
 
-def Categorys(request,type,searchtype):
+class Categorys(ListView):
+    
+    template_name = 'index.html'
+    paginate_by = 3
+    context_object_name = 'article'
 
-    try:
-        if searchtype==1:
-            article=Article.objects.select_related('category').filter(category__slug=type)
-        elif searchtype==2:
-            article=Article.objects.filter(tags__slug=type)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['author'] = Author.objects.all()
+        context['categorys'] = Category.objects.all()
+        return context
+
+    def get_queryset(self):
+        if self.kwargs['search_type'] == 1 :
+            self.search_category = get_object_or_404(Category, slug = self.kwargs['kinds'])
+            return Article.objects.filter(category = self.search_category)
+        elif self.kwargs['search_type'] == 2 :
+            return Article.objects.filter(tags__slug = self.kwargs['kinds'])
         else:
-            return Http404
-
-        author=Author.objects.all()
-
-        pagenate=Paginator(article,5)
-
-        if request.GET:
-            p=request.GET.get('page')
-            p=int(p)
-        else:
-            p=1
-
-        putart=pagenate.get_page(p)
-        page_num=pagenate.page_range
-        categ=Category.objects.all()
-        contents={
-            'author':author,
-            'article':putart,
-            'page_num':page_num,
-            'current_page':p,
-            'category':categ,
-        }
-
-        return render(request,'index.html',contents)
-
-    except (KeyError,Article.DoesNotExist,Category.DoesNotExist):
-        return Http404("お探しのコンテンツが見つかりませんでした。\n")
-    else:
-        return HttpResponse("障害が発生中です。\nお時間を置いてのアクセスをお願い致します。")
+            return Article.objects.all()
 
 def sitepolicy(request):
     policy=get_object_or_404(Policy,id=1)
